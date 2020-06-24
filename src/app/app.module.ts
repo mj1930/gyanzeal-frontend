cec11b74bd1e01b1e9c3b1ef62b90099f396c92c
mihir.jain-at-355914556489 <jainmihir84@gmail.com>
2020-06-24 22:18:56 +0530

    added header footer and login form teacher and student

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { StudentLoginComponent } from './student-login/student-login.component';
+import { TeacherLoginComponent } from './teacher-login/teacher-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    StudentLoginComponent,
+    TeacherLoginComponent
   ],
   imports: [
     BrowserModule,
